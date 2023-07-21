8e1dde9b9c5cd474b76e33f01bea795cfa55f348
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-07-21 22:30:20 +0530

    made the site responsive and added the hamburger menu-btn

--- js/script.js
+++ js/script.js
@@ -9,3 +9,38 @@ videoBtn.forEach(btn =>{
         document.querySelector('#video-slider').src = src;
     })
 })
+
+
+// --js code for event listner--
+
+let menuBtn = document.querySelector('.menu-btn');
+let navbar = document.querySelector('.navbar');
+let navLinks = document.querySelectorAll('.navbar a');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    navbar.classList.toggle('show');
+});
+
+// Added an event listener to each navigation link
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        if (navbar.classList.contains('show')) {
+            menuBtn.classList.toggle('active');
+            navbar.classList.remove('show');
+        }
+    });
+});
+
+// Added an event listener to the window to close the menu when clicking outside
+window.addEventListener('click', (event) => {
+    if (!event.target.closest('.navbar') && !event.target.closest('.menu-btn')) {
+        menuBtn.classList.remove('active');
+        navbar.classList.remove('show');
+    }
+});
+
+
+
+
+
